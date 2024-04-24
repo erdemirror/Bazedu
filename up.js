7878e000bd07146c442de6c44e94f723b0c0ac1a
mz310 <135813132+mz310@users.noreply.github.com>
2024-04-24 16:17:40 +0000

    added js

---
+++ up.js
@@ -0,0 +1,37 @@
+window.addEventListener('scroll', function() {
+    var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+    if (window.scrollY > 100) {
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+});
+
+document.getElementById('scrollToTopBtn').addEventListener('click', function() {
+    scrollToTop(0, 500);
+});
+
+function scrollToTop(to, duration) {
+    const start = window.scrollY;
+    const change = to - start;
+    let currentTime = 0;
+    const increment = 20;
+
+    function animateScroll() {
+        currentTime += increment;
+        const val = Math.easeInOutQuad(currentTime, start, change, duration);
+        window.scrollTo(0, val);
+        if (currentTime < duration) {
+            requestAnimationFrame(animateScroll);
+        }
+    }
+
+    animateScroll();
+}
+
+Math.easeInOutQuad = function(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+};
